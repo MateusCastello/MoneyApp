7e61a89c2778ab9b8d6bd224a561ab0789311b08
MateusCastello <mateus.sp@hotmail.com>
2019-11-14 11:07:33 -0300

    Primeiro commit

---
+++ app/src/main/java/br/unicamp/ft/f102312_m203257/MoneyApp/ui/send/SendViewModel.java
@@ -0,0 +1,19 @@
+package br.unicamp.ft.f102312_m203257.MoneyApp.ui.send;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
